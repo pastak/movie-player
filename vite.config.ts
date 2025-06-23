7d8b1228b9d2f0468795502d9a3982d53bff3b2a
pastak <pasta0915@gmail.com>
2025-06-24 02:22:15 +0900

    他のhtmlも含めるようにする

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-player/'
+  base: '/movie-player/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        player: 'player.html',
+        videoWithNotice: 'video-with-notice.html',
+        slide: 'slide.html',
+        sponsor: 'sponsor.html'
+      }
+    }
+  }
 })
